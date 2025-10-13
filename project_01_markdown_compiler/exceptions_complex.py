1ee65f57077c4005aa32258055f5d3a68fd6569f
AJ Holzer <aj.holzer@heschel.org>
2025-10-13 15:54:25 -0500

    Delete project_01_markdown_compiler directory

--- project_01_markdown_compiler/exceptions_complex.py
+++
@@ -1,9 +0,0 @@
-grades={
-    'alice':{'hw1':99,'hw2':88},
-    'bob':{'hw1':82,'hw2':91},
-    'charlie': {'hw2': 90}
-    }
-#sorted(grades.items())
-for k,v in sorted(grades.items()):
-    #print(k)
-    print(v['hw1'])
