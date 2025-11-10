416b483d463a98c7c878ee27356e67d63e8188d5
AJ Holzer <aj.holzer@heschel.org>
2025-11-10 12:52:22 -0500

    Add files via upload

---
+++ topic_03_Python_Containers/list_lab.py
@@ -0,0 +1,36 @@
+# List Lab Starter
+# Students: fill in code for Series 1–4 below
+
+def series1():
+    fruits = ["Apples", "Pears", "Oranges", "Peaches"]
+    print("Series 1:", fruits)
+    # your code continues here
+
+
+def series2():
+    # your code continues here, "pass" allows you to test your code with incomplete functions
+    pass
+
+
+def series3():
+    # your code continues here
+    pass
+
+
+def series4():
+    # your code continues here
+    pass
+
+
+def main():
+    series1()
+    series2()
+    series3()
+    series4()
+
+# this just tells Python:
+# "only run the code below if we are running THIS file directly"
+# (not if another file tries to import this file)
+# there is nothing to edit here
+if __name__ == "__main__":
+    main()
